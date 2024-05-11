6c321d7ead1f3f95cb2246786e33180901b2a199
Kavish_X <143331695+Kavishka-Rajapaksha@users.noreply.github.com>
2024-05-12 00:50:17 +0530

    logo with onboard one

--- app/src/main/java/com/example/mymanager/MainActivity.kt
+++ app/src/main/java/com/example/mymanager/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.mymanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Handler to delay navigation
+        Handler().postDelayed({
+            // Intent to navigate to OnboardActivity01
+            val intent = Intent(this, OnboardActivity01::class.java)
+            startActivity(intent)
+            // Finish current activity if you don't want to go back to it
+            finish()
+        }, 3000) // 3000 milliseconds = 3 seconds
     }
 }
---
+++ app/src/main/java/com/example/mymanager/OnboardActivity01.kt
@@ -0,0 +1,11 @@
+package com.example.mymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardActivity01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard01)
+    }
+}
